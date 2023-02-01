4a5d50648e1dbfaac7ac88a8b1fd169acf614fd3
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2023-02-01 11:06:15 -0300

    add keymaps options

--- lua/alemanoelsilva/keymaps.lua
+++ lua/alemanoelsilva/keymaps.lua
@@ -10,15 +10,15 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>qq", "<C-w>c") -- go to folder level
 
--- keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>dv", "<C-w>v") -- split window vertically
 -- keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
--- keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+-- keymap.set("n", "<leader>se","<C-w>=") -- make split windows equal width & height
 -- keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 -- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 -- keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
--- vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
--- keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+vim.keymap.set("n", "<leader>ta", ":tabn<CR>") --  go to next tab
+vim.keymap.set("n", "<leader>tb", ":tabp<CR>") --  go to previous tab
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 vim.keymap.set("n", "<leader>w", "<C-w>w") -- switch from tabs
